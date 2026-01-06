824ced95c8016b8d536b5c53133fb229a29e2fb9
nvrenuf <lee@leecuevas.com>
2026-01-06 15:59:05 -0700

    Set Astro output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
