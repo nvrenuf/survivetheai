4d0f5daf36fb873c648d2e322b969b9fcfbea932
nvrenuf <lee@leecuevas.com>
2026-01-06 15:48:21 -0700

    Add Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
     react(),
